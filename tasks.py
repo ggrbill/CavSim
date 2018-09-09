792a676b118f18b7f36421e8a3c8c13c25c43064
Gustavo G. Ribeiro <ggrbill@gmail.com>
2018-09-09 00:21:04 -0300

    Add invoke tasks. (development helpers)

---
+++ tasks.py
@@ -0,0 +1,77 @@
+from invoke import task 
+import os
+
+def get_project_name_and_root_for_cwd():
+    """
+    :return: The Name and root directory of the current project from the current working dir.
+    """
+    filename = 'environment.yml'
+    directory = os.path.abspath(os.curdir)
+    while True:
+        if os.path.exists(os.path.join(directory, filename)):
+            return os.path.basename(directory), directory.replace('\\', '/')
+
+        directory, current_dir_name = os.path.split(directory)
+        if len(current_dir_name) == 0:
+            raise RuntimeError(
+                'Can not obtain a project from the current working directory')
+        assert len(directory) != 0
+
+
+@task()
+def clean(ctx):
+	"""
+	Delete 'build' folder.
+	"""
+	project_name, project_pwd = get_project_name_and_root_for_cwd()
+	ctx.run('cd ' + project_pwd)
+
+	print("Cleaning!")
+	commands = [
+		'cd ' + project_pwd,
+		'rm -Rf build',
+	]
+	ctx.run(' && '.join(commands))
+
+
+@task(
+	help = {
+		'cclean': "Call Clean task (Delete 'build' folder) before build again."
+	}
+)
+def build(ctx, cclean=False):
+	"""
+	Build C++ code.
+	"""
+	if cclean:
+		clean(ctx)
+	
+	project_name, project_pwd = get_project_name_and_root_for_cwd()
+	
+	print("Building!")
+	commands = [
+		'cd ' + project_pwd,
+		'mkdir build',
+		'cd build',
+		'cmake ..',
+		'cmake --build .',
+	]
+	ctx.run(' && '.join(commands))
+	
+
+@task()
+def run_case_ex(ctx):
+	"""
+	Run an example case.
+	"""
+	project_name, project_pwd = get_project_name_and_root_for_cwd()
+	orig_folder = project_pwd + '/src/cpp/inCav.txt '
+	dest_folder = project_pwd + '/build/bin '
+
+	commands = [
+		'cp ' + orig_folder + dest_folder,
+		'cd ' + dest_folder,
+		'./' + project_name + ' inCav.txt',
+	]
+	print(commands)
+	ctx.run(' && '.join(commands))
