e4706b4bd0ac7f28ae926b97994bcdb523989e41
Gustavo G. Ribeiro <ggrbill@gmail.com>
2019-08-20 19:48:10 -0300

    Add read only  properties to CavitySetup binding

--- src/bindings/CavityPython.cpp
+++ src/bindings/CavityPython.cpp
@@ -5,6 +5,7 @@
 namespace py = pybind11;
 
 PYBIND11_MODULE(_CavSim, m) {
+    m.doc() = "CavSim Library";
     {
         using T = CavitySetup;  
         py::class_<T, std::shared_ptr<T> > c(m, "CavitySetup");
@@ -24,6 +25,17 @@ PYBIND11_MODULE(_CavSim, m) {
         py::arg("rho"),
         py::arg("mu"),
         py::arg("U_lid"));
+
+        c.def_readonly("L", &T::L);
+        c.def_readonly("H", &T::H);
+        c.def_readonly("dx", &T::dx);
+        c.def_readonly("dy", &T::dy);
+        c.def_readonly("n_x", &T::n_x);
+        c.def_readonly("n_y", &T::n_y);
+
+        c.def_readonly("rho", &T::rho);
+        c.def_readonly("mu", &T::mu);
+        c.def_readonly("U", &T::U);
     }
 }
 
